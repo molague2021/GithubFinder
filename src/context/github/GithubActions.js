91f8448587c86b58e8d4d42370e4e6e1eb805ba1
Martin Olague <molague23@hotmail.com>
2022-09-20 20:46:36 -0700

    Cleaning Up Our Actions and Axios

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,12 @@
+import axios from 'axios';
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  //headers: { Authorization: `token ${GITHUB_TOKEN}` },
+});
 
 // Get search users
 export const searchUsers = async (text) => {
@@ -7,47 +14,16 @@ export const searchUsers = async (text) => {
     q: text,
   });
 
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+  const response = await github.get(`/search/users?${params}`);
 
-  const { items } = await response.json();
-
-  return items;
+  return response?.data?.items;
 };
 
-// Get search users
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`);
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ]);
 
-  if (response.status === 404) {
-    window.location = '/notfound';
-  } else {
-    const data = await response.json();
-
-    return data;
-  }
-};
-
-// Get user repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: 'created',
-    per_page: 10,
-  });
-
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
-
-  const data = await response.json();
-
-  return data;
-};
-
-// FETCH USERS: Get initial users for testing purposes
-const fecthUsers = async () => {
-  const response = await fetch(`${GITHUB_URL}/users`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
-
-  const data = await response.json();
+  return { user: user.data, repos: repos.data };
 };
